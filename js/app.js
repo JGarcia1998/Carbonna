ec960bd162aec4c0a60f0c6d5b7726650c6dc638
JGarcia1998 <jared_garcia@aol.com>
2020-10-07 22:32:03 -0500

    created add to cart and remove functionality

--- js/app.js
+++ js/app.js
@@ -1,16 +1,41 @@
 let gridItems = document.querySelectorAll(".grid-item");
-let footer = document.querySelector(".footer");
+let ul = document.querySelector(".footer-container");
+let toDelete = false;
+let num = 0;
+let uniqueId = 0;
 
 gridItems.forEach((item) => {
   const button = item.querySelector(".add-btn");
   button.addEventListener("click", () => {
+    ul.style.visibility = "visible";
+    ul.style.opacity = "1";
     item.classList.add("animation");
-    footer.innerHTML = `<ul class="footer-container">
-    <li class="footer-items"></li>
-  </ul>`;
+    num += Number(item.querySelector(".item-price").getAttribute("value"));
+    document.querySelector(".fixed-cart-price").innerHTML = `$${num}`;
+    uniqueId++;
 
     setTimeout(() => {
       item.classList.remove("animation");
     }, 5000);
+
+    ul.insertAdjacentHTML(
+      "beforeend",
+      `<li value="${uniqueId}" class="footer-items">
+      <button class="remove-item">X</button>
+      <div class="side-by-side">     
+      ${
+        item.querySelector(".title").textContent
+      } <img class="cart-image" src="${item
+        .querySelector(".bike-image")
+        .getAttribute("src")}"/></div> <span class="cart-price">${
+        item.querySelector(".item-price").textContent
+      }</span> </li>`
+    );
+
+    document.querySelectorAll(".remove-item").forEach((item) => {
+      item.addEventListener("click", function () {
+        item.parentNode.remove();
+      });
+    });
   });
 });
